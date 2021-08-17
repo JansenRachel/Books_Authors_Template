08a429e2b76b2e51fbea2aaca4db5fbe4a94deb0
Rachel <rachel.jansen0613@gmail.com>
2021-08-17 15:45:33 -0500

    completed assignment, needs css styling, but forms and redirects work

--- book_authors_proj/book_authors_app/urls.py
+++ book_authors_proj/book_authors_app/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index),  #home- books page
+    path('create_book', views.create_book),   #adds a book to books page
+    path('view_book/<int:book_id>', views.view_book), #views one books info on book_display page
+    path('add_author', views.add_author),#adds an author from existing db to a book on book_display page
+
+    path('authors', views.authors),
+    path('create_author', views.create_author),
+    path('view_author/<int:author_id>', views.view_author), #views one books info on book_display page
+    path('add_book', views.add_book)
 ]
--- book_authors_proj/book_authors_app/views.py
+++ book_authors_proj/book_authors_app/views.py
@@ -1,6 +1,60 @@
-from django.shortcuts import render
+from django.db import models
+from django.shortcuts import redirect, render
+from .models import Book, Author
 
-# Create your views here.
+# Books 
 def index(request):
-    return render(request, "home.html")
-    
+    context = {
+        'all_books' : Book.objects.all(),
+        # 'authors' : Author.objects.all()
+    }
+    return render(request, "books.html", context)
+
+def create_book(request):
+    if request.method == "POST":
+        Book.objects.create (title = request.POST['title'], desc = request.POST['desc'])
+    return redirect('/')
+
+
+def view_book(request, book_id):
+    context = {
+        'book' : Book.objects.get(id = book_id),
+        'authors' : Book.objects.get(id = book_id).authors.all(),
+        'all_authors' : Author.objects.all(),
+        'book_id' : book_id
+    }
+    return render(request, "book_display.html", context)
+
+def add_author(request):
+    newAuth = Author.objects.get(id = request.POST['author'])
+    authsBook = Book.objects.get(id = request.POST['book'])
+    authsBook.authors.add(newAuth)
+    return redirect (f"view_book/{authsBook.id}")
+
+
+# Authors
+def authors(request):
+    context = {
+        'all_authors' : Author.objects.all(),
+    }
+    return render(request, "authors.html", context)
+
+def create_author(request):
+    if request.method == "POST":
+        Author.objects.create (first_name = request.POST['first_name'], last_name = request.POST['last_name'], notes = request.POST['notes'])
+    return redirect('/authors')
+
+def view_author(request, author_id):
+    context = {
+        'author' : Author.objects.get(id = author_id),
+        'book' : Author.objects.get (id = author_id).books.all(),
+        'all_books' : Book.objects.all(),
+        'author_id' : author_id
+    }
+    return render(request, "author_display.html", context)
+
+def add_book(request):
+    newBook = Book.objects.get(id = request.POST['book'])
+    booksAuth = Author.objects.get(id = request.POST['author'])
+    booksAuth.books.add(newBook)
+    return redirect (f"view_author/{booksAuth.id}")
